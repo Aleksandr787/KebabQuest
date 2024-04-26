9f825553bcea33660ebf0d4431174e915af3a47e
Artush-Baghdasaryan <artush.baghdasaryan@mail.ru>
2024-04-27 01:54:56 +0300

    chat gpt service rafactor

--- Backend/KebabQuest.Services/Services/AIModels/ChatGptProxyService.cs
+++ Backend/KebabQuest.Services/Services/AIModels/ChatGptProxyService.cs
@@ -8,7 +8,16 @@ using System.Text.Json;
 
 namespace KebabQuest.Services.Services.AIModels;
 
-public class ChatGptProxyService
+public class ChatGptProxyService : BaseChatGptService
 {
-    
+    private readonly ChatGptProxySettings _settings;
+    public ChatGptProxyService(IOptions<ChatGptProxySettings> settings) : base(new()
+    {
+        Url = settings.Value.Url,
+        ApiKey = settings.Value.ApiKey,
+        Model = settings.Value.Model
+    })
+    {
+        _settings = settings.Value;
+    }
 }
