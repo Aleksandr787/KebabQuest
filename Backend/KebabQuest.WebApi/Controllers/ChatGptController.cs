9743083424359412e27eb7755f55848b2baf05b9
Artush-Baghdasaryan <artush.baghdasaryan@mail.ru>
2024-04-27 02:14:06 +0300

    chat gpt controller

---
+++ Backend/KebabQuest.WebApi/Controllers/ChatGptController.cs
@@ -0,0 +1,32 @@
+﻿using KebabQuest.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KebabQuest.WebApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ChatGptController : ControllerBase
+{
+    private readonly IChatGptProxyService _chatGptProxyService;
+    private readonly IChatGptThebService _chatGptThebService;
+    
+    public ChatGptController(IChatGptProxyService chatGptProxyService, IChatGptThebService chatGptThebService)
+    {
+        _chatGptProxyService = chatGptProxyService;
+        _chatGptThebService = chatGptThebService;
+    }
+    
+    //for testing
+    [HttpPost("chat-proxy")]
+    public async Task<IActionResult> ChatProxy([FromQuery] string prompt)
+    {
+        var answer = await _chatGptProxyService.SendRequest(prompt);
+        return Ok(answer);
+    }
+
+    [HttpPost("chat-theb")]
+    public async Task<IActionResult> ChatTheb([FromQuery] string prompt)
+    {
+        var answer = await _chatGptThebService.SendRequest(prompt);
+    }
+}
