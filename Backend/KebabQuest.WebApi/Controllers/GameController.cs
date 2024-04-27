bb69e3520e138615121e210a149d937764272792
bathduck <bathduck@DESKTOP-VHCHCCD>
2024-04-27 03:38:58 +0300

    Удаление апи для теста GameRoomService

--- Backend/KebabQuest.WebApi/Controllers/GameController.cs
+++ Backend/KebabQuest.WebApi/Controllers/GameController.cs
@@ -17,45 +17,6 @@ namespace KebabQuest.WebApi.Controllers
         }
 
 
-        [HttpPost("[action]")]
-        public async Task<ActionResult<string>> CreateGameRoom(GameRoom newRoom)
-        {
-            try
-            {
-                string id = await _gameRoomService.CreateGameRoom(newRoom);
-                return Ok(id);
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [HttpPost("[action]")]
-        public async Task<ActionResult> DeleteGameRoom(string id)
-        {
-            try
-            {
-                await _gameRoomService.DeleteGameRoom(id);
-                return Ok();
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
-
-        [HttpPost("[action]")]
-        public async Task<ActionResult<GameRoom>> Get(string id)
-        {
-            try
-            {
-                return Ok(await _gameRoomService.GetById(id));
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(ex.Message);
-            }
-        }
+        
     }
 }
